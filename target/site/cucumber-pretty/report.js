c4d181d42c4d51831f005eef543bc2da0e01b1b6
ademkarapinar <ademkarapinar80@gmail.com>
2020-08-03 02:22:06 -0400

    git remote add origin https://github.com/ademkarapinar/BDDCucumberFramework.git

---
+++ target/site/cucumber-pretty/report.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Messenger.feature");
+formatter.feature({
+  "line": 3,
+  "name": "Messenger page feature",
+  "description": "",
+  "id": "messenger-page-feature",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@web"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6355035815,
+  "status": "passed"
+});
+formatter.background({
+  "line": 5,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 6,
+  "name": "I am on messenger page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "MessengerSD.setMessengerPage()"
+});
